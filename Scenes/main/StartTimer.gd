1b07c16ea79d373da0acd9161b14f0194246ff0f
augusto-roct <augusto.roct@gmail.com>
2024-02-06 08:48:46 -0300

    add: scene main

---
+++ Scenes/main/StartTimer.gd
@@ -0,0 +1,11 @@
+extends Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position = Vector2(240, 450)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
