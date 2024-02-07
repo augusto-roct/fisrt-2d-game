585b18f48e3ac87bc361ec0ee8a4a500f57132ab
augusto-roct <augusto.roct@gmail.com>
2024-02-07 09:11:15 -0300

    remove: visible screen entered

--- Scenes/mob/mob.gd
+++ Scenes/mob/mob.gd
@@ -28,10 +28,5 @@ func _process(delta):
 	position += velocity * delta
 
 
-func _on_visible_on_screen_notifier_2d_screen_entered():
-	#queue_free()
-	pass
-
-
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
